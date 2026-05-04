65597f85aa74f2b8230251c5415202c9432edb64
Asgardianxyz <sabanalcarlervin09@gmail.com>
2026-05-04 21:50:59 +0800

    Update 002_Exploring_NYC_Public_Schools_Test_Result_Scores.py

--- 002_Exploring_NYC_Public_Schools_Test_Result_Scores.py
+++ 002_Exploring_NYC_Public_Schools_Test_Result_Scores.py
@@ -8,24 +8,17 @@ schools = pd.read_csv("schools.csv")
 schools.head()
 
 # Start coding here...
+# Add as many cells as you like...
 
-# Which schools are best for math?
-best_math_schools = schools[schools["average_math"] >= 640][["school_name", "average_math"]].sort_values("average_math", ascending=False)
+best_math_schools = schools[schools["average_math"] >= 0.8*800][["school_name", "average_math"]].sort_values("average_math", ascending=False)
+print(best_math_schools)
 
-# Calculate total_SAT per school
 schools["total_SAT"] = schools["average_math"] + schools["average_reading"] + schools["average_writing"]
+top_10_schools = schools[["school_name", "total_SAT"]].sort_values("total_SAT", ascending=False).head(10)
+print(top_10_schools)
 
-# Who are the top 10 performing schools?
-top_10_schools = schools.sort_values("total_SAT", ascending=False)[["school_name", "total_SAT"]].head(10)
-
-# Which NYC borough has the highest standard deviation for total_SAT?
 boroughs = schools.groupby("borough")["total_SAT"].agg(["count", "mean", "std"]).round(2)
-
-# Filter for max std and make borough a column
 largest_std_dev = boroughs[boroughs["std"] == boroughs["std"].max()]
-
-# Rename the columns for clarity
-largest_std_dev = largest_std_dev.rename(columns={"count": "num_schools", "mean": "average_SAT", "std": "std_SAT"})
-
-# Optional: Move borough from index to column
+largest_std_dev = largest_std_dev.rename(columns={"count": "num_schools", "mean": "average_SAT", "std":"std_SAT"})
 largest_std_dev.reset_index(inplace=True)
+print(largest_std_dev)
